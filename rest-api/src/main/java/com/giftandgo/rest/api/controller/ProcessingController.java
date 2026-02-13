284b0a9d2211152ebb748d9bf626867fcafa558c
Edward Ellis <edwardellis.egre@gmail.com>
2026-02-13 22:22:01 +0000

    Can upload file and receive the same file unprocessed as a response.

--- rest-api/src/main/java/com/giftandgo/rest/api/controller/ProcessingController.java
+++ rest-api/src/main/java/com/giftandgo/rest/api/controller/ProcessingController.java
@@ -1,20 +1,49 @@
 package com.giftandgo.rest.api.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class ProcessingController {
 
     @RequestMapping(value="/process", method= RequestMethod.POST)
-    public String process(@RequestParam("input")MultipartFile input) {
-        log.info("Processing new input file.");
-        log.info("Processed input file.");
-        return "Processed";
+    public ResponseEntity process(@RequestParam("input")MultipartFile input, HttpServletRequest request) {
+        UUID requestId = UUID.randomUUID();
+        log.info("Processing new input file trace: {}", requestId);
+        File tempFile;
+        try {
+            tempFile = File.createTempFile(requestId.toString(), ".txt");
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        byte[] data;
+        try (FileOutputStream tempOutput = new FileOutputStream(tempFile)) {
+            data = input.getBytes();
+            log.info("File {} size {}", requestId, data.length);
+            tempOutput.write(data);
+            tempOutput.flush();
+        } catch(IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        log.info("Processed input file trace: {} at {}", requestId, tempFile.getAbsolutePath());
+
+        InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(data));
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"OutcomeFile.json\"")
+                .body(resource);
     }
 }
